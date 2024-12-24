a24e008490217ca9eada874dc8617ce65cca49dc
salim <salim.hype@gmail.com>
2024-12-24 20:36:19 +0600

    froms section middel of 20 min

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-from .models import Tour
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 # Create your views here.
 
-def index(request):
-    tours = Tour.objects.all()
-    contexts = {
-        'tours': tours
-    }
-    return render(request, 'tours/index.html', contexts)
+def home_view(request):
+    return render(request, 'tours/index.html')
+
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+    else:
+        form = ContactForm()
+    context = {'form': form}
+    return render(request, 'tours/contact.html', context)
+
+def contact_success_view(request):
+    return render(request, 'tours/contact_success.html')
